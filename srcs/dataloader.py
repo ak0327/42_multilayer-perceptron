9c1618b3e29a9185aa43511c718ef3872115b9bb
takira <ca.fie.0220@gmail.com>
2024-10-13 19:55:52 +0900

    add: dataloader and test_dataloader

---
+++ srcs/dataloader.py
@@ -0,0 +1,120 @@
+import numpy as np
+import pandas as pd
+from itertools import product
+
+
+def load_wdbc_data():
+    csv_path = "data/data.csv"
+    df = pd.read_csv(csv_path, header=None)  # todo: error
+
+    columns = ['id', 'diagnosis']
+    # 特徴量の名前リスト
+    features = [
+        'radius',
+        'texture',
+        'perimeter',
+        'area',
+        'smoothness',
+        'compactness',
+        'concavity',
+        'concavePoints',
+        'symmetry',
+        'fractal_dimension'
+    ]
+    # 統計量の名前リスト
+    stats = ['mean', 'stderr', 'worst']
+
+    for feature, stat in product(features, stats):
+        columns.append(f"{feature}_{stat}")
+
+    df.columns = columns
+
+    y = df['diagnosis']
+    X = df.drop('diagnosis', axis=1)
+    return X, y
+
+
+def _stratified_split(
+        y: pd.Series,
+        test_size: float,
+        stratify: pd.Series,
+        shuffle: bool,
+        rng
+) -> tuple[np.ndarray, np.ndarray]:
+    unique_classes, class_counts = np.unique(stratify, return_counts=True)
+    test_indices = []
+    train_indices = []
+
+    for cls, count in zip(unique_classes, class_counts):
+        class_indices = np.where(stratify == cls)[0]
+        if shuffle:
+            class_indices = rng.permutation(class_indices)
+
+        n_class_test = int(count * test_size)
+        test_class_indices = class_indices[:n_class_test]
+        train_class_indices = class_indices[n_class_test:]
+
+        test_indices.extend(test_class_indices)
+        train_indices.extend(train_class_indices)
+
+    return np.array(train_indices), np.array(test_indices)
+
+
+def _random_split(
+        n_total: int,
+        n_test: int,
+        shuffle: bool,
+        rng
+) -> tuple[np.ndarray, np.ndarray]:
+    if shuffle:
+        indices = rng.permutation(n_total)
+    else:
+        indices = np.arange(n_total)
+
+    test_indices = indices[:n_test]
+    train_indices = indices[n_test:]
+    return train_indices, test_indices
+
+
+def train_test_split(
+        X: pd.DataFrame,
+        y: pd.Series,
+        train_size: float,
+        shuffle: bool = False,
+        random_state: int = None,
+        stratify: pd.Series = None
+) -> tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]:
+    if not (0.0 < train_size and train_size < 1.0):
+        raise ValueError(
+            f"ValueError: "
+            f"The train_size={train_size}, should be 0.0 < valid_size < 1.0")
+    valid_size = 1.0 - train_size
+
+    n_total = len(X)
+    n_valid = int(n_total * valid_size)
+    rng = np.random.RandomState(random_state)
+
+    if stratify is not None:
+        train_indices, valid_indices = _stratified_split(y, valid_size, stratify, shuffle, rng)
+    else:
+        train_indices, valid_indices = _random_split(n_total, n_valid, shuffle, rng)
+
+    X_train, X_valid = X.iloc[train_indices], X.iloc[valid_indices]
+    y_train, y_valid  = y.iloc[train_indices], y.iloc[valid_indices]
+    return X_train, X_valid, y_train, y_valid
+
+
+def get_wdbc(
+        train_size: float,
+        shuffle: bool = False,
+        random_state: int = None
+):
+    X, y = load_wdbc_data()  # todo: error
+    X_train, X_valid, y_train, y_valid = train_test_split(
+        X=X,
+        y=y,
+        train_size=train_size,
+        shuffle=shuffle,
+        stratify=y
+    )
+    return X_train, X_valid, y_train, y_valid
