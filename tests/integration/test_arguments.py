7ae6b489277cfd6e1dd57f3137544643f8853308
takira <ca.fie.0220@gmail.com>
2024-10-20 10:52:58 +0900

    add(test): argument of train

--- tests/integration/test_arguments.py
+++ tests/integration/test_arguments.py
@@ -7,8 +7,10 @@ from srcs import dataloader, train, predict
 def dict_to_argv(file, arg_dict):
     argv = [file]
     for key, value in arg_dict.items():
-        if value is not None:
-            argv.extend([f'--{key}', str(value)])
+        if value is None:
+            continue
+        argv.append(f'--{key}')
+        argv.append(str(value))
     return argv
 
 
@@ -68,3 +70,99 @@ class TestDataloaderArgument:
         sys.argv = dict_to_argv(self.filename, valid_args)
         args = dataloader.parse_arguments()
         assert getattr(args, field) == expected
+
+
+class TestTrainArgument:
+    @classmethod
+    def setup_class(cls):
+        cls.base_args = {
+            'dataset_path':     '../data/data_train.csv',
+            'hidden_features':  '50 30',
+            'epochs':           '2000',
+            'learning_rate':    '0.0001',
+            'weight_decay':     '0.001',
+            'optimizer':        'Adam',
+            'verbose':          'false',
+            'plot':             'false',
+            'metrics_interval': '10',
+            'patience':         '100',
+            'save_dir':         '../data',
+        }
+        cls.filename = 'train.py'
+
+    def test_train_arguments(self):
+        sys.argv = dict_to_argv(self.filename, self.base_args)
+        args = train.parse_arguments()
+        assert args.dataset_path == '../data/data_train.csv'
+        assert args.hidden_features == [50, 30]
+        assert args.epochs == 2000
+        assert args.learning_rate == 0.0001
+        assert args.weight_decay == 0.001
+        assert args.optimizer == 'ADAM'
+        assert not args.verbose
+        assert not args.plot
+        assert args.metrics_interval == 10
+        assert args.patience == 100
+        assert args.save_dir == '../data'
+
+    @pytest.mark.parametrize("field, value, expected_error", [
+        ('dataset_path',    None,           SystemExit),
+        ('dataset_path',    '',             SystemExit),
+        ('dataset_path',    ' ',            SystemExit),
+        ('dataset_path',    'nothing',      SystemExit),
+        ('dataset_path',    'notcsv.txt',   SystemExit),
+
+        ('hidden_features', '',     SystemExit),
+        ('hidden_features', ' ',    SystemExit),
+        ('hidden_features', '50',   SystemExit),
+        ('hidden_features', '10 20 30 40 50 60',  SystemExit),
+        ('hidden_features', '1 1 1',            SystemExit),
+        ('hidden_features', '0 50 50',          SystemExit),
+        ('hidden_features', '-1 50 50',         SystemExit),
+        ('hidden_features', '10.0 20.0 30.0',   SystemExit),
+        ('hidden_features', '201 10 10',        SystemExit),
+        ('hidden_features', 'abc',              SystemExit),
+        ('hidden_features', '10 20 30 40 50a',  SystemExit),
+        ('hidden_features', '10 20 30 40 nan',  SystemExit),
+
+        ('learning_rate',   '',     SystemExit),
+        ('learning_rate',   ' ',    SystemExit),
+        ('learning_rate',   '0',    SystemExit),
+        ('learning_rate',   '-0.1', SystemExit),
+        ('learning_rate',   '1.2',  SystemExit),
+        ('learning_rate',   'inf',  SystemExit),
+        ('learning_rate',   'nan',  SystemExit),
+
+        ('weight_decay',    '',     SystemExit),
+        ('weight_decay',    ' ',    SystemExit),
+        ('weight_decay',    '-0.1', SystemExit),
+        ('weight_decay',    '1.2',  SystemExit),
+        ('weight_decay',    'inf',  SystemExit),
+        ('weight_decay',    'nan',  SystemExit),
+
+        ('optimizer',       '',     SystemExit),
+        ('optimizer',       ' ',    SystemExit),
+        ('optimizer',       'x',    SystemExit),
+
+        ('verbose',         '',     SystemExit),
+        ('verbose',         ' ',    SystemExit),
+        ('verbose',         'yes',  SystemExit),
+
+        ('plot',            '',     SystemExit),
+        ('plot',            ' ',    SystemExit),
+        ('plot',            'yes',  SystemExit),
+
+        ('metrics_interval',   '',      SystemExit),
+        ('metrics_interval',   '0',     SystemExit),
+        ('metrics_interval',   '-100',  SystemExit),
+        ('metrics_interval',   '-1000', SystemExit),
+        ('metrics_interval',   'nan',   SystemExit),
+        ('metrics_interval',   'inf',   SystemExit),
+
+        ('save_dir',    None,  SystemExit), ])
+    def test_invalid_arguments(self, field, value, expected_error):
+        invalid_args = self.base_args.copy()
+        invalid_args[field] = value
+        sys.argv = dict_to_argv(self.filename, invalid_args)
+        with pytest.raises(expected_error):
+            train.parse_arguments()
